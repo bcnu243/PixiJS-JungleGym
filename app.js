4836b07a31766668e1bce99c365ce59382d18c59
bcnu243 <theonlybcnu@hotmail.com>
2018-04-23 15:02:36 -0500

    Updated for Pixel Animation

--- app.js
+++ app.js
@@ -20,7 +20,21 @@ function setup() {
   stage.interactive = true;
 
 
-  var rect = new PIXI.rectange()
+  var rect = new PIXI.Rectangle(0,910,120,130)
+  var texture = PIXI.loader.resources["spritesheet"].texture;
+  texture.frame = rect;
+
+  sprite = new PIXI.Sprite(texture);
+
+  var idle = setInterval(function() {
+    if (rect.x >= 120 * 10) rect.x = 0;
+
+    sprite.texture.frame = rect;
+    rect.x += 120;
+  }, 100);
+
+  sprite.vx = 3;
+  stage.addChild(sprite);
   /*face = new PIXI.Sprite(
     PIXI.loader.resources["face"].texture
   );
@@ -50,4 +64,9 @@ function animationloop() {
 
   renderer.render(stage);
 }
+
+window.addEventListener("keydown", function(event) {
+  event.preventDefault();
+  sprite.x += sprite.vx
+});
 //test
